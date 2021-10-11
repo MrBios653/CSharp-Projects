b09ae2e886bcac6f0fd63e80ba504eeb49a67d1a
anonymous3446 <76919938+anonymous3446@users.noreply.github.com>
2021-10-11 13:20:57 +0300

    Add files via upload

---
+++ console-programlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Adınızı giriniz: ");
+            string name = Console.ReadLine();
+            Console.Write("Soyadınızı giriniz: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba "+ name + " " + surname);
+        }
+    }
+}
