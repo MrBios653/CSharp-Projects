73986df02b2042c25dcf775b79758a5f06bf8c42
MrBios653 <76919938+MrBios653@users.noreply.github.com>
2021-10-15 11:33:16 +0300

    Add files via upload

---
+++ while_foreach/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir sayı giriniz : ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while(sayac<=sayi)
+            {
+                toplam+=sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+
+            char character = 'a';
+            while(character < 'z')
+            {
+                Console.Write(character);
+                character ++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("***** Foreach *****");
+            string[] arabalar = {"BMW", "Nissan", "Toyota", "Ferrari"};
+
+            foreach(var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+
+        }
+    }
+}
