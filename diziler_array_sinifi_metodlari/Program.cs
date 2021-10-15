74eb00a276a775e58bac12363bc457958b8799d7
MrBios653 <76919938+MrBios653@users.noreply.github.com>
2021-10-15 15:58:56 +0300

    Add files via upload

---
+++ diziler_array_sinifi_metodlari/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace diziler_array_sinifi_metodlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayiDizisi = {5, 99, 34, 67, 34, 3, 9};
+            foreach(var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            Console.WriteLine("***** Sıralı Dizi *****");
+            Array.Sort(sayiDizisi);
+            
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            //Clear
+            Console.WriteLine("***** Array Clear *****");
+            Array.Clear(sayiDizisi, 2, 2);
+            foreach(var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            //Reverse
+            Console.WriteLine("***** Array Reverse *****");
+            Array.Reverse(sayiDizisi);
+            foreach(var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            //IndexOf
+            Console.WriteLine("***** Array IndexOf *****");
+            Console.WriteLine(Array.IndexOf(sayiDizisi, 99));
+
+            //Resize
+            Console.WriteLine("***** Array Resize *****");
+            Array.Resize<int>(ref sayiDizisi, 10);
+            sayiDizisi[7] = 100;
+            sayiDizisi[9] = 200;
+            foreach(var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+
+        }
+    }
+}
