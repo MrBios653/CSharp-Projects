d3241e73722a66fcb862fc7a038bbf882664e0eb
MrCPU64 <76919938+MrCPU64@users.noreply.github.com>
2021-10-28 18:43:28 +0300

    Add files via upload

---
+++ arraylist/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace arraylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList liste = new ArrayList();
+            /*liste.Add("Ali");
+            liste.Add(2);
+            liste.Add(true);
+            liste.Add('A');
+
+            // içerisinde verilere erişim
+            Console.WriteLine(liste[1]);
+
+            foreach (var item in liste)
+                Console.WriteLine(item);
+
+            */
+
+            // AddRange : toplu olarak eklemek
+            Console.WriteLine("***** AddRange *****");
+            //string[] renkler = {"kırmızı", "sarı", "yesil"};
+            List<int> sayilar = new List<int>(){9, 5, 4, 12, 34};
+            liste.AddRange(sayilar);
+            //liste.AddRange(renkler);
+
+            foreach (var item in liste)
+                Console.WriteLine(item);
+
+            // Sort
+
+            Console.WriteLine("***** Sort *****");
+            liste.Sort();
+
+            foreach (var item in liste)
+                Console.WriteLine(item);
+
+            // Binary search
+
+            Console.WriteLine("***** Binary Search *****");
+            Console.WriteLine(liste.BinarySearch(12));
+
+            // Reverse
+
+            Console.WriteLine("***** Reverse *****");
+            liste.Reverse();
+            
+            foreach (var item in liste)
+                Console.WriteLine(item);
+
+            // Clear
+            Console.WriteLine("***** Clear *****");
+            liste.Clear();
+
+            foreach (var item in liste)
+                Console.WriteLine(item);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
