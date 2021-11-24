633bd96b90ae13bf88d70f5635010bf4744c4d0d
MrCPU64 <76919938+MrCPU64@users.noreply.github.com>
2021-11-24 18:50:36 +0300

    Add files via upload

---
+++ sinif_kavrami/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace sinif_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calisan1 = new Calisan();
+            calisan1.Ad = "Ali";
+            calisan1.Soyad = "Yılmaz";
+            calisan1.No = 23678962;
+            calisan1.Departman = "İnsan Kaynakları";
+            calisan1.CalisanBilgileri();
+
+            Console.WriteLine("************************");
+
+            Calisan calisan2 = new Calisan();
+            calisan2.Ad = "Mehmet";
+            calisan2.Soyad = "Dursun";
+            calisan2.No = 56832193;
+            calisan2.Departman = "Satın Alma";
+            calisan2.CalisanBilgileri();
+
+        }
+    }
+
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Çalışanın Adı : {0}",Ad );
+            Console.WriteLine("Çalışanın Soyadı : {0}", Soyad);
+            Console.WriteLine("Çalışanın Numarası : {0}",No);
+            Console.WriteLine("Çalışanın Departmanı : {0}", Departman);
+
+        }
+    }
+}
